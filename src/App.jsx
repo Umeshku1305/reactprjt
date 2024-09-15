f81b86747bda3c146312150048e0aa502e07baa0
Umesh kumar mahto <umeshkumarmahto1305@gmail.com>
2024-09-15 18:36:18 +0530

    text added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 const App = () => {
   return (
-    <div className="mt-5 w-[80%] m-auto">
-      <h1 className="text-5xl mt-10 font-semibold">hello guys this is the react bolierplate </h1>
+    <div className="mt-5 w-[80%]  m-auto">
+      <h1 className="text-5xl mt-10 font-semibold text-red-600">hello guys this is the react bolierplate </h1>
+      <h1 className="text-4xl font-semibold mt-4 text-blue-900">hello guys ye hai hamara react ka bolierplate </h1>
       <h2 className="text-3xl">{import.meta.env.VITE_API_KEY}</h2>
       <button className="px-5 py-2 bg-blue-300 rounded mt-4 font-semibold ">Explore</button>
     </div>
